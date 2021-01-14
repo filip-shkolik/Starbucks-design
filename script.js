1c8c430924409743c3f2c6b4f322eee3b43a0584
Filip Shkolik <55874929+filip-shkolik@users.noreply.github.com>
2021-01-14 21:20:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function imgSlider(glass){
+    document.querySelector('.starbucks').src = glass;
+}
+
+function changeCircleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
